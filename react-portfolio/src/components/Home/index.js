9e275b41be32c98e3645ad5f7b3bff4858cceca0
Lewis20030124 <114333332+Lewis20030124@users.noreply.github.com>
2024-06-13 00:57:32 +1000

    make it better

--- react-portfolio/src/components/Home/index.js
+++ react-portfolio/src/components/Home/index.js
@@ -1,14 +1,22 @@
 import { Link } from 'react-router-dom';
 import LogoTitle from '../../assets/images/logo-s.png';
+import { useState } from 'react';
+import AnimatedLetters from '../AnimatedLetters';
 import './index.scss';
 
 const Home = () => {
+    const [letterClass, setLetterClass] = useState('text-animate');
+    const nameArray = ['l', 'e', 'w', 'i', 's'];
+    const titleArray = ['d', 'e', 'v', 'e', 'l', 'o', 'p', 'e', 'r'];
+
     return (
         <div className="container home-page">
             <div className="text-zone">
                 <h1>Hi, <br/> I'm
                 <img src={LogoTitle} alt="developer"/> 
-                Lewis
+                <AnimatedLetters letterClass={letterClass}
+                strArray={nameArray}
+                idx={15} />
                 <br/>
                 A Computer Science Graduate
                 </h1> 
