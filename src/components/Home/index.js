cf27de8d240e290af703b66c59e2ef0c9e0e4fef
Lewis20030124 <114333332+Lewis20030124@users.noreply.github.com>
2024-06-13 21:53:08 +1000

    Changed the time when the effect start to take place

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -16,7 +16,7 @@ const Home = () => {
       useEffect(() => {
         const timeoutId = setTimeout(() => {
             setLetterClass('text-animate-hover');
-        }, 4000);
+        }, 6000);
 
         // Cleanup function to clear the timeout
         return () => clearTimeout(timeoutId);
