32894773e53559744df164dfea740bc2da86949c
Lewis20030124 <114333332+Lewis20030124@users.noreply.github.com>
2024-06-13 23:43:40 +1000

    updated a logo image

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -3,6 +3,7 @@ import LogoTitle from '../../assets/images/L.png';
 import { useEffect, useState } from 'react';
 import AnimatedLetters from '../AnimatedLetters';
 import './index.scss';
+import Logo from './Logo';
 
 const Home = () => {
     const [letterClass, setLetterClass] = useState('text-animate');
@@ -46,6 +47,7 @@ const Home = () => {
                 </h2>
                 <Link to="/contact" className='flat-button'>CONTACT ME</Link>
             </div>
+            <Logo />
         </div>
     )
 }
